5ff68e0494190646737cb3acd1f1ec8542b815c2
Magdalena Yordanova <maggi.jordanova@gmail.com>
2024-07-03 19:40:14 +0000

    final filtering for object_detection_saver

--- team6_stack/scripts/object_detection_saver.py
+++ team6_stack/scripts/object_detection_saver.py
@@ -4,6 +4,8 @@ import rospy
 import os
 import csv
 import time
+import glob
+import pandas as pd
 
 from std_msgs.msg import Header
 from object_detection_msgs.msg import ObjectDetectionInfoArray
@@ -15,7 +17,7 @@ def callback(msg):
     for info in msg.info:
         info = {
             'class_id': info.class_id,
-            # TODO: Add 'id' if it's not always 0
+            'id': info.id,
             'x': info.position.x, # in camera frame
             'y': info.position.y, # in camera frame
             'z': info.position.z # in camera frame
@@ -37,8 +39,34 @@ def callback(msg):
         with open(file_path, mode='a', newline='') as file:
             writer = csv.writer(file)
             for info in infos:
-                writer.writerow([info['class_id'], info['x'], info['y'], info['z']])
-    
+                writer.writerow([info['class_id'], info['id'], info['x'], info['y'], info['z']])
+
+def shutdown_hook():
+    print("Shutting down node and performing cleanup.")
+
+    # Take the latest saved CSV file
+    dirname = os.path.dirname(__file__)
+    file_path = os.path.join(dirname, './../data/')
+    list_of_files = glob.glob(os.path.join(file_path, "*.csv"))
+    if list_of_files:
+        latest_file = max(list_of_files, key=os.path.getctime)
+        print(f"Processing the last saved CSV file: {latest_file}")
+
+        # Final filtering and clustering
+
+        # Load the CSV file into a pandas DataFrame
+        df = pd.read_csv(latest_file)
+        
+        # Calculate the average of 'x', 'y', and 'z' for each 'class_id' and 'id' combination
+        avg_df = df.groupby(['class_id', 'id']).agg({'x': 'mean', 'y': 'mean', 'z': 'mean'}).reset_index()
+        
+        # Save the filtered data to a new CSV file
+        file_name = 'artifacts-' + timestr + '-clustered.csv' # Add the already set timestamp + being clustered flag
+        avg_file_path = os.path.join(file_path, file_name)
+        avg_df.to_csv(avg_file_path, index=False)
+        
+        print(f"Filtered data saved to: {avg_file_path}")
+
 def object_detection_saver():
     rospy.init_node('object_detection_saver', anonymous=True)
 
@@ -56,12 +84,15 @@ def object_detection_saver():
     if not file_exists:
         with open(file_path, mode='w', newline='') as file:
             writer = csv.writer(file)
-            writer.writerow(['class_id', 'x', 'y', 'z'])
+            writer.writerow(['class_id', 'id', 'x', 'y', 'z'])
+
+    # Register the shutdown hook
+    rospy.on_shutdown(shutdown_hook)
 
     # Subscribe to topics
-    #rospy.Subscriber('/object_detector/detection_info', ObjectDetectionInfoArray, callback)
+    #rospy.Subscriber('/object_detector/detection_info', ObjectDetectionInfoArray, callback) # For independent testing
     rospy.Subscriber('/object_inspector/unique_artifacts', ObjectDetectionInfoArray, callback)
-    
+
     # Keep the script running
     rospy.spin()
 
